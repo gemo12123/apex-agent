3cfe1d5079851d4777df97f3c04ab4eced8f740c
gemo <1151641026@qq.com>
2026-04-24 10:43:08 +0800

    解决唯一键冲突并取消重试

--- apex-agent/src/main/java/org/gemo/apex/hook/NoOpAgentHookRuntime.java
+++ apex-agent/src/main/java/org/gemo/apex/hook/NoOpAgentHookRuntime.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.LinkedHashMap;
 
-@Component
+//@Component
 public class NoOpAgentHookRuntime implements AgentHookRuntime {
 
     @Override
